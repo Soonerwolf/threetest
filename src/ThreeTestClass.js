5b4308238e39cd8580c5f86fdc1177374a4bd325
Brad Stanley <brad@mesonet.org>
2025-09-26 11:08:13 -0500

    Project: using Vite; index.html: module section now working; ThreeTestClass: fixed dangling renderer issues.

--- src/ThreeTestClass.js
+++ src/ThreeTestClass.js
@@ -4,12 +4,12 @@ export class ThreeTest
 {
     constructor(inContainer)
     {
-        this.renderer = new THREE.WebGlRenderer();
-        renderer.setSize(inContainer.offsetWidth, inContainer.offsetHeight);
-        inContainer.appendChild(renderer.domElement);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(inContainer.offsetWidth, inContainer.offsetHeight);
+        inContainer.appendChild(this.renderer.domElement);
 
-        this.scene = new THREE.scene();
-        this.camera = new THREE.PerspectiveCamera(45, inContainer.offsetWidth, inContainer.offsetHeight, 1, 4000);
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(45, inContainer.offsetWidth / inContainer.offsetHeight, 1, 4000);
         this.camera.position.set(0,0,3.333);
 
         let geometry = new THREE.PlaneGeometry(1,1);
